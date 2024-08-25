18f57e5e5437143b93950d53a09e76415ddf99b5
Lucas Rial <111540106+LucasRialx@users.noreply.github.com>
2024-08-25 12:07:23 -0300

    Update CryptoTxCalculator.js
    acrescentei uma regra para os prejuizos.

--- CryptoTxCalculator.js
+++ CryptoTxCalculator.js
@@ -3,21 +3,27 @@ const prompt = require('prompt-sync')();
 // Função para calcular o imposto devido.
 function calcularImposto(rendaBruta, custo, taxaImposto) {
     const lucro = rendaBruta - custo;
-    const impostoDevido = lucro * taxaImposto;
+    const impostoDevido = lucro > 0 ? lucro * taxaImposto : 0;
     return impostoDevido;
 }
 
-// Solicita as informações para o usuario.
+// Solicita as informações para o usuário.
 console.log("Bem-vindo ao calculador de imposto para transações de criptomoedas!");
 
-// As perguntas para o usuario.  
+// As perguntas para o usuário.
 const rendaBruta = parseFloat(prompt('Qual foi o valor total da venda de criptomoedas? '));
 const custo = parseFloat(prompt('Qual foi o custo total das criptomoedas compradas? '));
 
-// Taxa de imposto, neste caso vou tratar apenas a aliquota de 15%.
+// Taxa de imposto, neste caso vou tratar apenas a alíquota de 15%.
 const taxaImposto = 0.15;
 
 // Calculando o imposto que será pago pela movimentação.
 const impostoDevido = calcularImposto(rendaBruta, custo, taxaImposto);
+const lucro = rendaBruta - custo;
 
-console.log(`O imposto devido sobre o lucro é: R$ ${impostoDevido.toFixed(2)}`);
+// Exibindo o resultado com base no lucro.
+if (lucro > 0) {
+    console.log(`O imposto devido sobre o lucro é: R$ ${impostoDevido.toFixed(2)}`);
+} else {
+    console.log('Você teve um prejuízo. Não há imposto a pagar.');
+}
